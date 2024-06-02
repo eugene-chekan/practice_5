dd095939173c9e3cd6c1cdd60dc7af32c0ea145d
Yauheni Chekan <eugene.chekan@gmail.com>
2024-06-02 18:37:21 +0200

    Add annotations and comments

--- client.c
+++ client.c
@@ -15,17 +15,52 @@ int fd_recv = -1;
 int fd_send = -1;
 int cur_sum = 0;
 bool user2_exit = false;
+char buffer[BUFFER_SIZE];
+int bytes_read;
 
+/**
+ * Opens a file descriptor for the given FIFO channel.
+ *
+ * @param fchannel The name of the FIFO channel.
+ * @param mode The mode in which to open the FIFO.
+ * @return The file descriptor for the opened FIFO.
+ */
 int open_fd(char *fchannel, int mode);
+
+/**
+ * Closes the given file descriptor.
+ *
+ * @param fd The file descriptor to close.
+ */
 void close_fd(int fd);
+
+/**
+ * Signal handler for SIGUSR1.
+ *
+ * @param signum The signal number.
+ */
 void handle_sigusr1(int signum);
+
+/**
+ * Signal handler for SIGUSR2.
+ *
+ * @param signum The signal number.
+ */
 void handle_sigusr2(int signum);
+
+/**
+ * Cleanly exits the program by closing file descriptors and terminating.
+ *
+ * @param exit_status The status code to exit with.
+ */
+void clean_exit(int exit_status);
 
 
 int main(int argc, char *argv[]) {
+    // Check for correct number of arguments
     if (argc != 3) {
         fprintf(stderr, "Usage: %s <fifo_send> <fifo_recv>\n", argv[0]);
-        exit(EXIT_FAILURE);
+        clean_exit(EXIT_FAILURE);
     }
 
     fifo_send = argv[1];
@@ -35,20 +70,23 @@ int main(int argc, char *argv[]) {
     signal(SIGUSR1, handle_sigusr1);
     signal(SIGUSR2, handle_sigusr2);
     
+    // Open the receive FIFO in non-blocking read mode
     fd_recv = open_fd(fifo_recv, O_RDONLY | O_NONBLOCK);
 
+    // Initialize a timeout structure for the select call.
     struct timeval timeout;
     timeout.tv_sec = 1;
     timeout.tv_usec = 0;
 
+    // Initialize a file descriptor set for select.
     fd_set read_fds;
     FD_ZERO(&read_fds);
     FD_SET(fd_recv, &read_fds);
 
-    char buffer[BUFFER_SIZE];
-    int bytes_read;
+    // Main loop to read data from the receive FIFO
     while (!user2_exit) {
 	timeout.tv_sec = 1;
+        // Use select to wait for data on the receive FIFO, read the data, and sum the digits.
     	if (select(fd_recv + 1, &read_fds, NULL, NULL, &timeout) <= 0) {
 	        continue;
     	}
@@ -57,7 +95,8 @@ int main(int argc, char *argv[]) {
         }
         while ((bytes_read = read(fd_recv, buffer, sizeof(buffer) - 1)) > 0) {
             buffer[bytes_read] = '\0';
-
+            
+            // Process each character in the buffer, parse digits and add to sum
             for (const char *p = buffer; *p; p++) {
                 if (isdigit(*p)) {
                     cur_sum += *p - '0';
@@ -66,15 +105,16 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    // Prepare and send the result when user2_exit is true
     char result[BUFFER_SIZE];
     snprintf(result, sizeof(result), "%d: %d", getpid(), cur_sum);
     if (fd_send < 0) {
         fd_send = open_fd(fifo_send, O_WRONLY);
     }
     write(fd_send, result, strlen(result));
-    close_fd(fd_recv);
-    close_fd(fd_send);
-    exit(EXIT_SUCCESS);
+
+    // Clean up and finish the program
+    clean_exit(EXIT_SUCCESS);
 }
 
 void handle_sigusr1(int signum) {
@@ -95,7 +135,7 @@ int open_fd(char *fchannel, int mode) {
     int fd;
     if ((fd = open(fchannel, mode)) == -1) {
         fprintf(stderr, "Client failed to open %s channel\n", fchannel);
-        exit(EXIT_FAILURE);
+        clean_exit(EXIT_FAILURE);
     }
     return fd;
 }
@@ -103,6 +143,12 @@ int open_fd(char *fchannel, int mode) {
 void close_fd(const int fd) {
     if (close(fd) == -1) {
         perror("Client failed to close fd");
-        exit(EXIT_FAILURE);
+        clean_exit(EXIT_FAILURE);
     }
+}
+
+void clean_exit(int exit_status) {
+    close_fd(fd_recv);
+    close_fd(fd_send);
+    exit(exit_status);
 }
--- server.c
+++ server.c
@@ -27,13 +27,45 @@ char **write_fifos;
 char fifo_in[BUFFER_SIZE], fifo_out[BUFFER_SIZE];
 char out_buf[BUFFER_SIZE], in_buf[BUFFER_SIZE];
 
+/**
+ * Opens a file descriptor for the given FIFO channel.
+ *
+ * @param fchannel The name of the FIFO channel.
+ * @param mode The mode in which to open the FIFO.
+ * @return The file descriptor for the opened FIFO.
+ */
 int open_fd(char *fchannel, int mode);
+
+/**
+ * Closes the given file descriptor.
+ *
+ * @param fd The file descriptor to close.
+ */
 void close_fd(int fd);
+
+/**
+ * Creates a FIFO (named pipe).
+ *
+ * @param fifo_name The name of the FIFO to create.
+ */
 void create_fifo(char *fifo_name);
+
+/**
+ * Removes a FIFO (named pipe).
+ *
+ * @param fifo_name The name of the FIFO to remove.
+ */
 void remove_fifo(char *fifo_name);
+
+/**
+ * Handles the SIGINT signal to stop the server.
+ *
+ * @param sig The signal number.
+ */
 void stop_server(int sig);
 
 int main(int argc, char *argv[]) {
+    // Check the number of arguments and initialize file_path and num_processes
     if (argc == 2) {
         file_path = "stdin";
         num_processes = atoi(argv[1]);
@@ -57,13 +89,16 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    // Allocate memory for FIFO file descriptors and names
     read_fds = malloc(num_processes * sizeof(int));
     write_fds = malloc(num_processes * sizeof(int));
     read_fifos = (char **)malloc(num_processes * sizeof(char *));
     write_fifos = (char **)malloc(num_processes * sizeof(char *));
 
+    // Register the SIGINT signal handler
     signal(SIGINT, stop_server);
 
+    // Create FIFOs and fork child processes
     for (int i = 0; i < num_processes; i++) {
         snprintf(fifo_in, sizeof(fifo_in), "fifo_in_%d", i);
         snprintf(fifo_out, sizeof(fifo_out), "fifo_out_%d", i);
@@ -80,7 +115,7 @@ int main(int argc, char *argv[]) {
             execl("./client", "./client", fifo_in, fifo_out, (char *)NULL);
             perror("Server failed to start client process\n");
             exit(EXIT_FAILURE);
-        } else {
+        } else { // Parent process
             pids[i] = pid;
             read_fifos[i] = strdup(fifo_in);
             write_fifos[i] = strdup(fifo_out);
@@ -89,7 +124,8 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    if (file_path == "stdin") {
+    // Open input file or use stdin
+    if (strcmp(file_path, "stdin") == 0) {
         input = stdin;
     } else {
         input = fopen(file_path, "r");
@@ -100,8 +136,7 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    // int times = 0;
-    // int process = 0;
+    // Main loop to read lines from the input and distribute them to child processes
     while (fgets(in_buf, sizeof(in_buf), input)) {
 	    if (write_fds[process] < 0) {
 		    write_fds[process] = open_fd(write_fifos[process], O_WRONLY);
@@ -134,7 +169,10 @@ int main(int argc, char *argv[]) {
         process = (process + 1) % num_processes;
     }
 
+    // Close input file
     fclose(input);
+
+    // Send SIGUSR2 to child processes to signal them to finish
     for (int i = 0; i < num_processes; i++) {
         if (kill(pids[i], SIGUSR2) < 0) {
             perror("Server failed to send SIGUSR2 signal\n");
@@ -148,11 +186,13 @@ int main(int argc, char *argv[]) {
         while ((bytes_read = read(read_fds[i], in_buf, sizeof(in_buf) - 1)) > 0) {
             in_buf[bytes_read] = '\0';
             printf("Final result from process %s\n", in_buf);
+            
             // Variables to store the extracted integers
             int proc_num, result;
             
             // Parse the input string
             if (sscanf(in_buf, "%d: %d", &proc_num, &result) == 2) {
+                
                 // Add the second integer to the sum
                 total_sum += result;
             } else {
@@ -161,11 +201,13 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    // Wait for all child processes to finish
     for (int i = 0; i < num_processes; i++) {
         waitpid(pids[i], NULL, 0);
         printf("Process %d finished.\n", pids[i]);
     }
 
+    // Clean up FIFOs and file descriptors
     for (int i = 0; i < num_processes; i++) {
         close_fd(read_fds[i]);
         close_fd(write_fds[i]);
@@ -175,12 +217,13 @@ int main(int argc, char *argv[]) {
     printf(LINE_SEPARATOR);
     printf("Final sum: %d\n", total_sum);
 
+    // Free allocated memory
     free(pids);
     free(read_fds);
     free(write_fds);
     free(read_fifos);
     free(write_fifos);
-    exit(0);
+    exit(EXIT_SUCCESS);
 }
 
 void create_fifo(char *fifo_name) {
